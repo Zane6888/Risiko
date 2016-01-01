d62eba17a7a051e143ed946563eb243c099fc78b
Simon Wohlfart <simon.wohlfart@gmx.at>
2016-01-01 19:18:35 +0100

    Replaced anonymous class with lambda

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,11 +6,9 @@ public class Main {
 
     public static void main(String[] args) {
         final String map = args.length > 0 ? args[0] : null;
-        EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                Window window = new Window(map);
-                window.setVisible(true);
-            }
+        EventQueue.invokeLater(() -> {
+            Window window = new Window(map);
+            window.setVisible(true);
         });
     }
 }
