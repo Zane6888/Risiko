4ada5149530a4dab405c4ddf2f26c71205010b6d
livus <theulminator5@gmail.com>
2016-01-01 12:58:59 +0100

    Updated Neighbors drawing logic

--- src/com/company/Neighbors.java
+++ src/com/company/Neighbors.java
@@ -20,16 +20,53 @@ public class Neighbors {
     public void paint(Graphics2D g) {
         Point start = neighborOne.getCapitalPosition();
         Point end = neighborTwo.getCapitalPosition();
-        if (Math.abs(start.x - end.x) > GameConstants.WINDOW_WIDTH / 2) { //if the start and end point are two far away draw the other side around
-            if (start.x < end.x) {
-                drawDashedLine(g, start, new Point(-end.x, end.y));
-                drawDashedLine(g, end, new Point(GameConstants.WINDOW_WIDTH + start.x, start.y));
-            } else {
-                drawDashedLine(g, start, new Point(GameConstants.WINDOW_WIDTH + end.x, end.y));
-                drawDashedLine(g, end, new Point(-start.x, start.y));
+        //if the start and end point are two far away draw the other side around
+        if (Math.abs(start.x - end.x) > GameConstants.WINDOW_WIDTH / 2) { //start and end x position are two far away
+            if (Math.abs(start.y - end.y) > GameConstants.WINDOW_HEIGHT / 2) { //x and y positions are two far away
+                if (start.x < end.x) {
+                    if (start.y < end.y) {
+                        drawDashedLine(g, start, new Point(-end.x, -end.y));
+                        drawDashedLine(g, end, new Point(GameConstants.WINDOW_WIDTH + start.x, GameConstants.WINDOW_HEIGHT + start.y));
+                    } else {
+                        drawDashedLine(g, start, new Point(-end.x, GameConstants.WINDOW_HEIGHT + end.y));
+                        drawDashedLine(g, end, new Point(GameConstants.WINDOW_WIDTH + start.x, -start.y));
+                    }
+
+                } else {
+                    if (start.y < end.y) {
+                        drawDashedLine(g, start, new Point(GameConstants.WINDOW_WIDTH + end.x, -end.y));
+                        drawDashedLine(g, end, new Point(-start.x, GameConstants.WINDOW_HEIGHT + start.y));
+                    } else {
+                        drawDashedLine(g, start, new Point(GameConstants.WINDOW_WIDTH + end.x, GameConstants.WINDOW_HEIGHT + end.y));
+                        drawDashedLine(g, end, new Point(-start.x, -start.y));
+                    }
+
+                }
+            } else { //x positions are two far away
+                if (start.x < end.x) {
+                    drawDashedLine(g, start, new Point(-end.x, end.y));
+                    drawDashedLine(g, end, new Point(GameConstants.WINDOW_WIDTH + start.x, start.y));
+                } else {
+                    drawDashedLine(g, start, new Point(GameConstants.WINDOW_WIDTH + end.x, end.y));
+                    drawDashedLine(g, end, new Point(-start.x, start.y));
+                }
             }
-            //TODO IF-Verzeigungen f�r die y-Koordinate fehlen noch
-        } else drawDashedLine(g, start, end);
+
+        } else {
+            if (Math.abs(start.y - end.y) > GameConstants.WINDOW_HEIGHT / 2) { //y positions are two far away
+                if (start.y < end.y) {
+                    drawDashedLine(g, start, new Point(end.x, -end.y));
+                    drawDashedLine(g, end, new Point(start.x, GameConstants.WINDOW_HEIGHT + start.y));
+                } else {
+                    drawDashedLine(g, start, new Point(end.x, GameConstants.WINDOW_HEIGHT + end.y));
+                    drawDashedLine(g, end, new Point(start.x, -start.y));
+                }
+
+            } else {
+                drawDashedLine(g, start, end);  //normal case, where the line is not drawn over the border
+            }
+
+        }
     }
 
     /**
